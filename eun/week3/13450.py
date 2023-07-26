3f3eb9378939997b3a5b16463c24998a1c639f3e
우은진 <esthrelar@ueunjins-MacBook-Air.local>
2023-07-27 08:58:16 +0900

    13450 solved

---
+++ eun/week3/13450.py
@@ -0,0 +1,27 @@
+#László Babai
+
+T = int(input())
+results = ['no'] * T
+
+for i in range(T):
+    gA = []
+    gB = []
+
+    m = int(input())
+    for j in range(m):
+        uv_set = set(list(map(int, input().split())))
+        gA.append(uv_set)
+
+    m_ = int(input())
+    for j in range(m_):
+        uv_set = set(list(map(int, input().split())))
+        gB.append(uv_set)
+
+    # vertex는 총 1,2,3 3개뿐이고 
+    # 두 버텍스 사이의 에지는 최대 하나만 입력하도록 가정하니,
+    # 에지의 수만 같으면 동형의 그래프가 된다.
+    if (m == m_): 
+        results[i] = 'yes'
+
+for i in range(len(results)):
+    print(results[i])
