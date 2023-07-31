36d10277249b7a6a7dea5fe9ad27b96dee70bce7
우은진 <esthrelar@ueunjins-MacBook-Air.local>
2023-07-31 10:19:12 +0900

    16236 solv..g

--- eun/week3/2/11404.py
+++ eun/week3/2/11404.py
@@ -13,37 +13,55 @@ for bus in range(m):
             if (dep == arr):
                 mincost[dep][arr] = 0
             else:
+                if (mincost[b][arr] != 0 and mincost[dep][a] == 0):
+                     if mincost[dep][arr] == 0:
+                        mincost[dep][arr] = c + mincost[b][arr]
+                     elif mincost[dep][arr] > c + mincost[b][arr]:
+                          mincost[dep][arr] = c + mincost[b][arr]
+                elif (mincost[b][arr] == 0 and mincost[dep][a] != 0):
+                    if mincost[dep][arr] == 0:
+                        mincost[dep][arr] = mincost[dep][a] + c
+                    elif mincost[dep][arr] > mincost[dep][a] + c:
+                            mincost[dep][arr] = mincost[dep][a] + c
+                elif (mincost[dep][a] != 0 and mincost[b][arr] != 0):
+                    if mincost[dep][arr] == 0:
+                        mincost[dep][arr] = mincost[dep][a] + c + mincost[b][arr]
+                    elif mincost[dep][arr] > mincost[dep][a] + c + mincost[b][arr]:
+                            mincost[dep][arr] = mincost[dep][a] + c + mincost[b][arr]
+                else:
+                     if mincost[dep][arr] == 0:
+                        mincost[dep][arr] = c
+                     elif mincost[dep][arr] > c:
+                        mincost[dep][arr] = c
+                """
                 # a -> b 인 경우
                 if (dep == a and arr == b):
                     if mincost[dep][arr] == 0:
                         mincost[dep][arr] = c
-                    else:
-                        if mincost[dep][arr] > c:
+                    elif mincost[dep][arr] > c:
                             mincost[dep][arr] = c
                 # a -> 다른 도착지인 경우
-                if (dep == a and arr != b):
+                elif (dep == a and arr != b):
                     if mincost[b][arr] != 0:
                         if mincost[dep][arr] == 0:
                             mincost[dep][arr] = c + mincost[b][arr]
-                        else:
-                            if mincost[dep][arr] > c + mincost[b][arr]:
+                        elif mincost[dep][arr] > c + mincost[b][arr]:
                                 mincost[dep][arr] = c + mincost[b][arr]
                 # 다른 출발지 -> b 인 경우
-                if (dep != a and arr == b):
+                elif (dep != a and arr == b):
                     if (mincost[dep][a] != 0):
                         if mincost[dep][arr] == 0:
                             mincost[dep][arr] = mincost[dep][a] + c
-                        else:
-                            if mincost[dep][arr] > mincost[dep][a] + c:
+                        elif mincost[dep][arr] > mincost[dep][a] + c:
                                 mincost[dep][arr] = mincost[dep][a] + c
                 # 다른 출발지 -> 다른 도착지인 경우
-                if (dep != a and arr != b):
+                elif (dep != a and arr != b):
                     if (mincost[dep][a] != 0 and mincost[b][arr] != 0):
                         if mincost[dep][arr] == 0:
                             mincost[dep][arr] = mincost[dep][a] + c + mincost[b][arr]
-                        else:
-                            if mincost[dep][arr] > mincost[dep][a] + c + mincost[b][arr]:
+                        elif mincost[dep][arr] > mincost[dep][a] + c + mincost[b][arr]:
                                 mincost[dep][arr] = mincost[dep][a] + c + mincost[b][arr]
+                """
 
 for i in range(n):
     for j in range(n):
