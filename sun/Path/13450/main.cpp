252b4f5d048275a785febdd5a86963bda9eb4cf8
Sunha-i <icecream20@khu.ac.kr>
2023-08-03 06:05:19 +0900

    solve 13450, 17129

--- sun/Path/13450/main.cpp
+++ sun/Path/13450/main.cpp
@@ -11,21 +11,26 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    int T, m, u, v;
+    int T, u, v;
+    int edge[2];
     cin >> T;
     
     for (int i = 0; i < T; i++)
     {
-        int e1 = 0;
-        int e2 = 0;
         for (int j = 0; j < 2; j++)
         {
-            int m;
-            cin >> m;
-            
+            cin >> edge[j];
+            for (int i = 0; i < edge[j]; i++)
+            {
+                cin >> u >> v;  // 그냥 날리기
+            }
         }
-        
+        if (edge[0] == edge[1])     cout << "yes\n";
+        else    cout << "no\n";
     }
     
     return 0;
 }
+
+// vertex의 개수가 최대 3개라 받은 edge 값들을 모두 사용하지 않아도 됨
+// 허접한 문제..
--- sun/Path/17129/main.cpp
+++ sun/Path/17129/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <cstring>  // memset
 #include <queue>
 
 int n, m;
@@ -14,12 +15,13 @@ int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
 int map[3001][3001];
-int dist[3001][3001];
+int dist[3001][3001] ;
 
 int bfs(int x, int y)
 {
     std::queue<std::pair<int,int>> que;
     que.push({x,y});
+    dist[x][y] = 0;
     while (!que.empty()) {
         int x = que.front().first;
         int y = que.front().second;
@@ -31,7 +33,7 @@ int bfs(int x, int y)
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                if (map[nx][ny] == 1)
+                if (map[nx][ny] == 1 || dist[nx][ny] >= 0)
                     continue;
                 
                 dist[nx][ny] = dist[x][y] + 1;
@@ -45,15 +47,17 @@ int bfs(int x, int y)
 int main(int argc, const char * argv[]) {
     
     std::cin >> n >> m;
-    std::string str;
+    char c;
+    
+    memset(dist, -1, sizeof(dist));
     
     int start[2];
     for (int i = 0; i < n; i++)
     {
-        std::cin >> str;
         for (int j = 0; j < m; j++)
         {
-            int val = static_cast<int>(str[j]) - '0';
+            std::cin >> c;
+            int val = static_cast<int>(c) - '0';
             if (val == 2) {
                 start[0] = i;
                 start[1] = j;
@@ -68,3 +72,6 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+// 내 기존 코드에서 dist를 -1로 초기화해주고 시작위치 0으로 설정,
+// 35 line에서 dist>=0일 때만 진입"의 조건을 추가했음
