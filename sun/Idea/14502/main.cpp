3d1f0f5c574c0306b3e8aa43dfbd198ac7905acf
Sunha-i <icecream20@khu.ac.kr>
2024-01-31 20:35:23 +0900

    add new solution

--- sun/Idea/14502/main.cpp
+++ sun/Idea/14502/main.cpp
@@ -3,26 +3,36 @@
 //  14502 연구소
 //
 //  Created by Sun on 2023/09/29.
+//       & updated on 2024/01/31.
 //
 
 #include <iostream>
-#include <vector>
+#include <algorithm>    // for copy
 #include <queue>
 
 using namespace std;
 
 int N, M;
 int map[8][8];
-int tmp_map[8][8];
-bool vstd[8][8] = {0,};
+int ans;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-void bfs(int x, int y) {
+void bfs() {
+    
+    int tmp[8][8];
+    copy(&map[0][0], &map[0][0]+64, &tmp[0][0]);
     
     queue<pair<int,int>> q;
-    q.push({x,y});
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] == 2) {
+                q.push({i,j});
+            }
+        }
+    }
     
     while (!q.empty()) {
         int x = q.front().first;
@@ -32,9 +42,33 @@ void bfs(int x, int y) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                if (tmp_map[nx][ny] != 0)   continue;
-                tmp_map[nx][ny] = 2;
+                if (tmp[nx][ny] != 0)   continue;
+                tmp[nx][ny] = 2;
                 q.push({nx,ny});
+            }
+        }
+    }
+    
+    int safearea = 0;
+    for (int n = 0; n < N; n++) {
+        for (int m = 0; m < M; m++) {
+            if (tmp[n][m] == 0) safearea++;
+        }
+    }
+    ans = max(ans, safearea);
+}
+
+void nCm(int cnt) {
+    if (cnt == 3) {     // 벽을 3개 세우면
+        bfs();          // bfs -> 바이러스 퍼트리기
+        return;
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] == 0) {
+                map[i][j] = 1;
+                nCm(cnt+1);
+                map[i][j] = 0;
             }
         }
     }
@@ -43,44 +77,13 @@ void bfs(int x, int y) {
 int main(int argc, const char * argv[]) {
     
     scanf("%d %d", &N, &M);
-
-    vector<pair<int,int>> blank;
-    vector<pair<int,int>> virus;
+    
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             cin >> map[i][j];
-            if (map[i][j] == 0)         blank.push_back({i,j});
-            else if (map[i][j] == 2)    virus.push_back({i,j});
         }
     }
-
-    int ans = 0;
-    for (int i = 0; i < blank.size(); i++) {
-        for (int j = 0; j < i; j++) {
-            for (int k = 0; k < j; k++) {
-                map[blank[i].first][blank[i].second] = 1;
-                map[blank[j].first][blank[j].second] = 1;
-                map[blank[k].first][blank[k].second] = 1;
-                
-                copy(&map[0][0], &map[0][0] + 64, &tmp_map[0][0]);
-                for (int v = 0; v < virus.size(); v++) {
-                    bfs(virus[v].first, virus[v].second);
-                }
-                
-                int safearea = 0;
-                for (int n = 0; n < N; n++) {
-                    for (int m = 0; m < M; m++) {
-                        if (tmp_map[n][m] == 0) safearea++;
-                    }
-                }
-                ans = max(ans, safearea);
-                
-                map[blank[i].first][blank[i].second] = 0;
-                map[blank[j].first][blank[j].second] = 0;
-                map[blank[k].first][blank[k].second] = 0;
-            }
-        }
-    }
+    nCm(0);
     cout << ans;
     
     return 0;
@@ -88,3 +91,6 @@ int main(int argc, const char * argv[]) {
 
 // 시간복잡도를 계산하거나 주어진 조건(가로세로 최대 8, 벽을 3개 세울 것)에서 힌트를 얻어
 // 조합으로 모든 경우를 탐색하는 문제임을 파악해야함. 그 이후 구현은 간단함
+
+// ++ 24/1/31 추가) 벽을 세우는 조합을 구하는 방법 수정
+// 빈칸0들 중에 3개를 뽑고 벽을 세운 다음, 그 상태에서 bfs를 호출 -> 매번 새로운 map 필요.
--- sun/Path/2667/main.cpp
+++ sun/Path/2667/main.cpp
@@ -1,27 +1,32 @@
 //
 //  main.cpp
-//  2667
+//  2667 단지번호붙이기
 //
 //  Created by Sun on 2023/09/11.
+//       & updated on 2024/01/31.
 //
 
 #include <iostream>
+#include <queue>
 #include <vector>
 #include <algorithm>    //sort
 
 using namespace std;
 
-int N, ans = 0;
-string map[25];
-bool vstd[25][25] = {0,};
+int N;
+char map[25][25];
+bool vstd[25][25];
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
+int house = 0;
+vector<int> complex;
+
 void dfs(int x, int y) {
     
     vstd[x][y] = true;
-    ans++;
+    house++;
     
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
@@ -33,40 +38,60 @@ void dfs(int x, int y) {
     }
 }
 
+void bfs(int x, int y) {
+    
+    if (map[x][y] == '0')   return;
+    
+    queue<pair<int,int>> q;
+    q.push({x,y});
+    vstd[x][y] = true;
+    house++;
+    
+    while (!q.empty()) {
+        int xx = q.front().first;
+        int yy = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = xx + dx[i];
+            int ny = yy + dy[i];
+            if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+                if (!vstd[nx][ny] && map[nx][ny] == '1') {
+                    q.push({nx,ny});
+                    vstd[nx][ny] = true;
+                    house++;
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
     cin >> N;
-    
-    for (int i = 0; i < N; i++) {
-        cin >> map[i];
-    }
-    
-    vector<int> block;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            if (map[i][j] == '1' && !vstd[i][j]) {
-                dfs(i, j);
-                block.push_back(ans);
-                ans = 0;
+            cin >> map[i][j];
+        }
+    }
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (map[i][j] == '1' && !vstd[i][j])
+            {
+                bfs(i, j);      // or dfs(i,j)
+                complex.push_back(house);
+                house = 0;
             }
         }
     }
     
-    cout << block.size() << endl;
-    sort(block.begin(), block.end());
-    for (int i = 0; i < block.size(); i++) {
-        cout << block[i] << endl;
+    cout << complex.size() << endl;
+    sort(complex.begin(), complex.end());
+    for (int i = 0; i < complex.size(); i++) {
+        cout << complex[i] << endl;
     }
     
     return 0;
 }
 
-// 이것도 아이스크림 문제
-// 반례 해결하기
-// -> 가 아니라 아래처럼 구현했기 때문에 25자리 숫자가 들어왔을 때 감당이 안됨
-// -> 그냥 string으로 해야함
-// for (int j = N-1; j >= 0; j--) {
-//     int r = line % 10;
-//     map[i][j] = r;
-//     line /= 10;
-// }
+// 아이스크림 문제
